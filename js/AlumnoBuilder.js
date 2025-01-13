2f3e22608497260fdd8bfa148bcf5dad4f824726
Yit-hub <yidanielchen.23@campuscamara.es>
2025-01-13 15:53:26 +0100

    Creación de Clase Alumno con patrón builder

--- js/AlumnoBuilder.js
+++ js/AlumnoBuilder.js
@@ -1,18 +1,72 @@
-class Alumno{
-    constructor(nombre,apellidos,nif,lengua,conocidos){
-        this.nombre = nombre;
-        this.apellidos = apellidos;
-        this.nif = nif;
-        this.lengua = lengua;
-        this.conocidos = conocidos;
+class Alumno {
+    constructor(builder) {
+        this.nombre = builder.nombre;
+        this.apellidos = builder.apellidos;
+        this.nif = builder.nif;
+        this.lenguaMaterna = builder.lenguaMaterna;
+        this.idiomasConocidos = builder.idiomasConocidos;
+        this.familiares = builder.familiares;
+        this.direccion = builder.direccion;
+        this.datosAcademicos = builder.datosAcademicos;
+        this.informacionMedica = builder.informacionMedica;
+    }
+
+    static get Builder() {
+        return class {
+            constructor() {
+                this.familiares = [];
+                this.idiomasConocidos = [];
+            }
+
+            setNombre(nombre) {
+                this.nombre = nombre;
+                return this;
+            }
+
+            setApellidos(apellidos) {
+                this.apellidos = apellidos;
+                return this;
+            }
+
+            setNIF(nif) {
+                this.nif = nif;
+                return this;
+            }
+
+            setLenguaMaterna(lengua) {
+                this.lenguaMaterna = lengua;
+                return this;
+            }
+
+            setIdiomasConocidos(idiomas) {
+                this.idiomasConocidos = idiomas;
+                return this;
+            }
+
+            addFamiliar(familiar) {
+                this.familiares.push(familiar);
+                return this;
+            }
+
+            setDireccion(direccion) {
+                this.direccion = direccion;
+                return this;
+            }
+
+            setDatosAcademicos(datosAcademicos) {
+                this.datosAcademicos = datosAcademicos;
+                return this;
+            }
+
+            setInformacionMedica(informacionMedica) {
+                this.informacionMedica = informacionMedica;
+                return this;
+            }
+
+            build() {
+                return new Alumno(this);
+            }
+        };
     }
 }
-class AlumnoBuilder{
-    constructor(){
-        this.nombre = null;
-        this.apellidos = null;
-        this.nif = null;
-        this.lengua = null;
-        this.conocidos = null;
-    }
-}
+
